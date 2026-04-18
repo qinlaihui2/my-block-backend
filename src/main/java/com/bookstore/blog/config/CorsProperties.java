52f3f0e5f2c7e1d81d41e7de1211333a92677ac1
qinlaihui2 <2188926911@qq.com>
2026-04-18 10:38:26 +0800

    Delete src/main directory

--- src/main/java/com/bookstore/blog/config/CorsProperties.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.blog.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import java.util.List;
-
-@ConfigurationProperties(prefix = "app.cors")
-public record CorsProperties(List<String> allowedOrigins) {
-}
--- src/main/java/com/bookstore/blog/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.blog.repository;
-
-import com.bookstore.blog.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
